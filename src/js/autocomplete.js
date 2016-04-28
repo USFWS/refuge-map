07ffe2892c566bb3125461eab6b9c2d8a709a28b
Roy Hewitt <roy_hewitt@fws.gov>
2016-04-28 10:59:59 -0400

    Factor out lunr from the autocomplete module

--- src/js/autocomplete.js
+++ src/js/autocomplete.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  var lunr = require('lunr');
+  var lunr = require('./search');
 
   var emitter = require('./mediator');
   var _ = require('./util');
@@ -16,7 +16,6 @@
   function init(options) {
     opts = _.defaults({}, options, defaults);
     registerHandlers();
-    createIndex();
   }
 
   function registerHandlers() {
@@ -32,22 +31,15 @@
       opts.input.value = refugeName;
       window.location.hash = _.slugify(refugeName);
       opts.list.innerHTML = '';
-      emitter.emit('selected:office', findRefugeByName(refugeName) );
+      emitter.emit('selected:office', refugeName);
     }
-  }
-
-  function findRefugeByName(name) {
-    var offices = _.filter(opts.data.features, function (refuge) {
-      return refuge.properties.ORGNAME.toLowerCase() === name.toLowerCase();
-    });
-    return offices[0];
   }
 
   function findRefuges(e) {
     var term = opts.input.value;
     if (term.length === 0) opts.list.innerHTML = '';
     if (term.length <= opts.minLength) return;
-    render(searchIndex(term));
+    render(lunr.search(term));
   }
 
   function render(data) {
@@ -55,48 +47,6 @@
       refuge.properties.slug = _.slugify(refuge.properties.ORGNAME);
     });
     opts.list.innerHTML = template(data);
-  }
-
-  function createIndex() {
-    index = lunr(function () {
-      this.field('ORGNAME', { boost: 10 });
-      this.field('ZIPCODE');
-      this.field('CITY');
-      this.field('STATE');
-      this.ref('ORGCODE');
-    });
-    _.each(opts.data.features, function(refuge) {
-      var props = refuge.properties;
-      index.add({
-        "ORGCODE": props.ORGCODE,
-        "ORGNAME": props.ORGNAME,
-        "CITY": props.CITY,
-        "STATE": props.STATE,
-        "ZIP": props.ZIP
-      });
-    });
-  }
-
-  function searchIndex(term) {
-    var filtered = [];
-    var results = index.search(term);
-    _.each(results, function(result) {
-      _.each(opts.data.features, function (refuge) {
-        refuge.score = result.score;
-        if (refuge.properties.ORGCODE === result.ref) filtered.push(refuge);
-      });
-    });
-    filtered.sort(sortByScore);
-    return filtered;
-  }
-
-  function sortByScore(a,b) {
-    if (a.score < b.score)
-      return -1;
-    else if (a.score > b.score)
-      return 1;
-    else
-      return 0;
   }
 
   module.exports.init = init;
