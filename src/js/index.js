aa614e0c763dcd4955d81d01f735195cefe2153f
Roy Hewitt <roy_hewitt@fws.gov>
2016-04-28 11:01:21 -0400

    Add event listener to toggle between views

--- src/js/index.js
+++ src/js/index.js
@@ -3,20 +3,40 @@
 
   var emitter = require('./mediator');
   var _ = require('./util');
+
   var data = require('./data');
+
   var map = require('./map');
   var autocomplete = require('./autocomplete');
+  var list = require('./list');
+  var search = require('./search');
+
+  var toggle = document.querySelector('.toggle-view');
+
+  toggle.addEventListener('click', toggleView);
+
+  function toggleView() {
+    emitter.emit('toggle:view', toggle);
+  }
 
   data.init();
 
   emitter.on('refuges', function (refuges) {
+
+    search.init({
+      data: refuges
+    });
+
+    list.init({
+      input: document.querySelector('.text-list-search'),
+      list: document.querySelector('.refuge-list')
+    });
 
     map.init({
       data: refuges
     });
 
     autocomplete.init({
-      data: refuges,
       input: document.querySelector('.map-autocomplete'),
       list: document.querySelector('.autocomplete-results')
     });
